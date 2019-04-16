989843347d3a78c48557b4e56ecaa59d7baf9f90
chuanhek98 <chuanhek98@gmail.com>
2019-04-16 16:14:35 +0800

    Link done

--- frameworks/js/myApp.js
+++ frameworks/js/myApp.js
@@ -3,3 +3,9 @@ db.collection('Members').get().then((snapshot) => {
         console.log(doc.data())
     })
 })
+
+db.collection('Staffs').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data())
+    })
+})
