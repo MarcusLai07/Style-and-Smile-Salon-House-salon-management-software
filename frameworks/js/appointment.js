fc2d5c2268af3608f9950c32b8465dc8df5add18
MarcusLai07 <37129492+MarcusLai07@users.noreply.github.com>
2019-04-19 18:58:52 +0800

    Update appointment.js

--- frameworks/js/appointment.js
+++ frameworks/js/appointment.js
@@ -13,10 +13,20 @@
 
 
 
-const AppointmentList = document.querySelector('#all_content');
+//Adding appoinment into database
 const form = document.querySelector('#add-appointment-form');
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	db.collection('Appointment').add({
+		customer_name: form.A_name.value,
+		services: form.A_services.value,
+		time: form.A_time.value,
+		date: form.A_date.value
+	})
+})
 
- 
+//Render all appoinments
+const AppointmentList = document.querySelector('#all_content');
 function renderAppointment(doc){
     let tr = document.createElement('tr');
     let A_name = document.createElement('td');
@@ -34,40 +44,18 @@ function renderAppointment(doc){
     tr.appendChild(A_services);
     tr.appendChild(A_time);
     tr.appendChild(A_date);
-	
-	
-    
+
     AppointmentList.append(tr);
-     
 }
 
-
 db.collection('Appointment').orderBy("date", "desc").get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-       
+    snapshot.docs.forEach(doc => { 
 		renderAppointment(doc);
     })
 })
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	db.collection('Appointment').add({
-		
-		customer_name: form.A_name.value,
-		
-		services: form.A_services.value,
-		
-		time: form.A_time.value,
-		
-		date: form.A_date.value
-		
-		
-	})
-})
-
-
+//Render today appoinments
 const TodayAppointmentList = document.querySelector('#today_content');
-
 function renderTodayAppointment(doc){
     let tr = document.createElement('tr');
     let A_name = document.createElement('td');
@@ -85,23 +73,18 @@ function renderTodayAppointment(doc){
     tr.appendChild(A_services);
     tr.appendChild(A_time);
     tr.appendChild(A_date);
-	
-	
-    
+
     TodayAppointmentList.append(tr);
-     
 }
 
-
 db.collection('Appointment').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-       
+    snapshot.docs.forEach(doc => {  
 		renderTodayAppointment(doc);
     })
 })
 
+//Render past appoinments
 const PreviousAppointmentList = document.querySelector('#previous_content');
-
 function renderPreviousAppointment(doc){
     let tr = document.createElement('tr');
     let A_name = document.createElement('td');
@@ -119,24 +102,18 @@ function renderPreviousAppointment(doc){
     tr.appendChild(A_services);
     tr.appendChild(A_time);
     tr.appendChild(A_date);
-	
-	
-    
+
     PreviousAppointmentList.append(tr);
-     
 }
 
-
 db.collection('Appointment').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-       
+    snapshot.docs.forEach(doc => {       
 		renderPreviousAppointment(doc);
     })
 })
 
-
+//Render upcoming appoinments
 const UpcomingAppointmentList = document.querySelector('#upcoming_content');
-
 function renderUpcomingAppointment(doc){
     let tr = document.createElement('tr');
     let A_name = document.createElement('td');
@@ -154,18 +131,12 @@ function renderUpcomingAppointment(doc){
     tr.appendChild(A_services);
     tr.appendChild(A_time);
     tr.appendChild(A_date);
-	
-	
-    
+
     UpcomingAppointmentList.append(tr);
-     
 }
 
-
 db.collection('Appointment').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-       
+    snapshot.docs.forEach(doc => { 
 		renderUpcomingAppointment(doc);
     })
 })
-
