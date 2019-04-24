ff0e2f577d960b58e06009e72e02cd5a3cd8fdfa
MarcusLai07 <37129492+MarcusLai07@users.noreply.github.com>
2019-04-24 15:23:30 +0800

    login stuff

--- frameworks/js/myApp.js
+++ frameworks/js/myApp.js
@@ -11,7 +11,27 @@
         const db = firebase.firestore();
         db.settings({timestampsInSnapshots: true})
 
-
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                console.log("you have signed in")
+                //get user information
+                var uid;
+                uid = user.uid;
+                console.log("User ID: " + user.uid);
+        
+                if (user.email == "admin@gmail.com") {    //if the user is the manager
+                    var content = '<ul class="nav flex-column nav-pills"><h5 class="sidebar-heading mt-5 px-3 py-3"><span>Reports & Analytics</span></h5><li class="nav-item"><a class="nav-link text-white" href="Generate_Report.html"><i class="fas fa-file-invoice mr-3"></i>Generate Report</a></li><li class="nav-item"><a class="nav-link text-white" href="Generate_Forecast.html"><i class="fas fa-file-contract mr-3"></i>Generate Analytics</a></li></ul>';
+                    document.getElementById("LogButton").innerHTML = "Manager Log Out";
+                    $('#navigationSide').append(content);
+                }
+                if (user.email != "admin@gmail.com") {  //if the user is the staff or not manager       
+                    document.getElementById("LogButton").innerHTML = "Staff Log Out"
+                }
+            } else {
+                console.log("Signed off");
+                window.location.replace("login.html")
+            }
+        })
 
 
 
