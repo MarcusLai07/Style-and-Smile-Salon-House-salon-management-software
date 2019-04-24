d679b2169b8a4c8ca32f1c4261dc604631ca779a
MarcusLai07 <37129492+MarcusLai07@users.noreply.github.com>
2019-04-24 22:57:42 +0800

    change login method

--- frameworks/js/login.js
+++ frameworks/js/login.js
@@ -1,44 +1,47 @@
-// Initialize Firebase
-var config = {
+var app_fireBase = {};
+(function(){
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyB8gd8MusZyqJ0vLQFkgoUX8E6A6RP5t_A",
     authDomain: "style-and-smile-salon-house.firebaseapp.com",
     databaseURL: "https://style-and-smile-salon-house.firebaseio.com",
     projectId: "style-and-smile-salon-house",
     storageBucket: "style-and-smile-salon-house.appspot.com",
     messagingSenderId: "1030007772704"
- };
- firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      console.log(user.email + " signed in");
-      if (user.email =="admin@gmail.com"){
-        window.location.replace("index.html");
+  app_fireBase = firebase;
+
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
       }
-  
-      if(user != null){
-        console.log("Welcome " + user.email);
-      }
-    } else {
-      // No user is signed in.
-      console.log("Not signed in")
-    }
-});
-  
-$(document).on("click", "#Loginbtn", function (){
-    var userEmail = document.getElementById("emailfield").value;
-    var userPass = document.getElementById("passwordfield").value;
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'index.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // // Terms of service url.
+    // tosUrl: 'index.html',
+    // // Privacy policy url.
+    // privacyPolicyUrl: 'index.html'
+  };
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
 
-      window.alert("Error : " + errorMessage);
-    });
-});
-  
-  //function logout(){
-  //  firebase.auth().signOut();
-  //}
+}());
