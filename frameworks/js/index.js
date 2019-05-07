ae3920fa38e1717bdb87939717702e650349bfa4
MarcusLai07 <37129492+MarcusLai07@users.noreply.github.com>
2019-05-07 22:04:54 +0800

    framework updated

--- frameworks/js/index.js
+++ frameworks/js/index.js
@@ -1,22 +1,27 @@
 var inDex = {};
 
-(function(){
-
-    //logout module
-    var firebase = app_fireBase;
-    var uid = null;
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            user = user.uid;
-        }else{
-            uid = null;
-            window.location.replace("login.html");
-        }
+// Material Select Initialization
+$(document).ready(function() {
+    $('.mdb-select').materialSelect();
+    $('.select-wrapper.md-form.md-outline input.select-dropdown').bind('focus blur', function () {
+    $(this).closest('.select-outline').find('label').toggleClass('active');
+    $(this).closest('.select-outline').find('.caret').toggleClass('active');
     });
-    function logOut(){
-        firebase.auth().signOut();
-    }
-    inDex.logOut = logOut;
+});
 
-})()
+//logout module
+var firebase = app_fireBase;
+var uid = null;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        user = user.uid;
+    }else{
+        uid = null;
+        window.location.replace("login.html");
+    }
+});
+function logOut(){
+    firebase.auth().signOut();
+}
+inDex.logOut = logOut;
