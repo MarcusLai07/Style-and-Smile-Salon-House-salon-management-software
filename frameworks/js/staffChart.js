cabb6d0b4ebb426cbeb2d841a75569fe109cd608
Daniel Wong <danielwong9898@gmail.com>
2019-05-12 17:34:41 +0800

    made StaffReport page with chart (dummy data)

---
+++ frameworks/js/staffChart.js
@@ -0,0 +1,70 @@
+/*jslint white:true*/
+/*global angular*/
+
+
+//from interface design lecture 8
+
+var chart=Highcharts.chart('myChart',{
+    
+    chart:{
+        type:'pie'
+    },
+    plotOptions:{
+         series: {
+         cursor: 'pointer',
+         events: {
+             click: function () {
+                    alert('666');
+                }
+            }
+        }
+    },
+    
+    title:{
+        text:'Style and Smile Salon House Yearly Staff Performance'
+    },
+    
+    
+//    It is an pie chart, so xAxis and yAxis does not do anything here. Logically.
+//    xAxis:{
+//        categories:['Chuan Hek', 'Marcus Lai', 'Daniel Wong', 'Gimmy Sii', 'Wong'],
+//        title:{
+//            enabled:true,
+//            text:"Staff Name"
+//        }
+//    },
+//    
+//    yAxis:{
+//         title:{
+//            enabled:true,
+//            text:"Total Services Sales"
+//        } 
+//    },
+    
+       series: [{
+            name: 'Total Sales in RM',
+            colorByPoint: true,
+            data: [{
+                name: 'Chuan Hek',
+                y: 11222,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Marcus Lai',
+                y: 22333
+            }, {
+                name: 'Daniel Wong',
+                y: 44555
+            }, {
+                name: 'Gimmy Sii',
+                y: 55566
+            }, {
+                name: 'Wong',
+                y: 77888
+            }],
+        }]
+   
+     
+});
+
+
