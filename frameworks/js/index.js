8be058289a9e25025235169d3c6afe50fb66fac0
MarcusLai07 <37129492+MarcusLai07@users.noreply.github.com>
2019-05-14 15:04:50 +0800

    update on sales page

--- frameworks/js/index.js
+++ frameworks/js/index.js
@@ -12,31 +12,118 @@ var config = {
     const db = firebase.firestore();
     db.settings({timestampsInSnapshots: true})
 
+    const Summary = document.querySelector('#PurchaseSummary');
+
 function getSelectedService(){
     var selectedValue = document.getElementById("S_Content").value;
     var selectedText = document.getElementById("S_Content").options[S_Content.selectedIndex].innerHTML;
-    console.log(selectedValue + selectedText);
 
-    listNode = document.getElementById('S_List');
-    liNode = document.createElement("LI");
-    liNode.className = "list-group-item";
-    txt = document.createTextNode(selectedText + " RM" + selectedValue);
+    let tr = document.createElement('tr');
+    tr.className = "text-center";
 
-    liNode.appendChild(txt);
-    listNode.appendChild(liNode);
+    let S_txt = document.createElement('td');
+    let S_value = document.createElement('td');
+    var btnEdit=document.createElement("BUTTON");
+    btnEdit.innerHTML="Edit";
+    btnEdit.className="btn btn-outline-info btn-xs";
+    var btnDelete=document.createElement("BUTTON");
+    btnDelete.innerHTML="Delete";
+    btnDelete.className="btn btn-outline-danger btn-xs";
+
+    Stxt = document.createTextNode(selectedText);
+    Svalue = document.createTextNode("RM"+selectedValue);
+
+    S_txt.appendChild(Stxt);
+    S_value.appendChild(Svalue);
+    tr.appendChild(S_txt);
+    tr.appendChild(S_value);
+    tr.appendChild(btnEdit);
+    tr.appendChild(btnDelete);
+
+    Summary.append(tr);
+
+    btnEdit.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log("heres your id that you selected: "+ id);
+         selectedID=id;
+         console.log(selectedID);
+         modal_Edit.style.display="block";
+         span.onclick=function()
+         {
+             modal_Edit.style.display="none";
+         }
+         window.onclick=function(event)
+         {
+             if(event.target==modal_Edit)
+                 {
+                     modal_Edit.style.display="none";
+                 }
+         }
+    })
+    btnDelete.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log("heres your id that you selected: "+ id);
+        db.collection('Members').doc(id).delete();
+           alert("You had sucessfully delete the item from system! Your table will now be updated!");
+    })
+
 }
 function getSelectedItem(){
     var selectedValue = document.getElementById("I_Content").value;
     var selectedText = document.getElementById("I_Content").options[I_Content.selectedIndex].innerHTML;
-    console.log(selectedValue + selectedText);
 
-    listNode = document.getElementById('S_List');
-    liNode = document.createElement("LI");
-    liNode.className = "list-group-item";
-    txt = document.createTextNode(selectedText + " RM" + selectedValue);
+    let tr = document.createElement('tr');
+    tr.className = "text-center";
 
-    liNode.appendChild(txt);
-    listNode.appendChild(liNode);
+    let S_txt = document.createElement('td');
+    let S_value = document.createElement('td');
+    var btnEdit=document.createElement("BUTTON");
+    btnEdit.innerHTML="Edit";
+    btnEdit.className="btn btn-outline-info btn-xs";
+    var btnDelete=document.createElement("BUTTON");
+    btnDelete.innerHTML="Delete";
+    btnDelete.className="btn btn-outline-danger btn-xs";
+
+    Stxt = document.createTextNode(selectedText);
+    Svalue = document.createTextNode("RM"+selectedValue);
+
+    S_txt.appendChild(Stxt);
+    S_value.appendChild(Svalue);
+    tr.appendChild(S_txt);
+    tr.appendChild(S_value);
+    tr.appendChild(btnEdit);
+    tr.appendChild(btnDelete);
+
+    Summary.append(tr);
+
+    btnEdit.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log("heres your id that you selected: "+ id);
+         selectedID=id;
+         console.log(selectedID);
+         modal_Edit.style.display="block";
+         span.onclick=function()
+         {
+             modal_Edit.style.display="none";
+         }
+         window.onclick=function(event)
+         {
+             if(event.target==modal_Edit)
+                 {
+                     modal_Edit.style.display="none";
+                 }
+         }
+    })
+    btnDelete.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log("heres your id that you selected: "+ id);
+        db.collection('Members').doc(id).delete();
+           alert("You had sucessfully delete the item from system! Your table will now be updated!");
+    })
 }
 
 //logout module
